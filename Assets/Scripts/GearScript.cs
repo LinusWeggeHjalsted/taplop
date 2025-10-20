38a1e05714ce529a1a590cb9a325c0e58791a951
narkokran <linus_dk@hotmail.com>
2025-10-20 18:07:37 +0200

    first draft of second level, and some cleanup

--- Assets/Scripts/GearScript.cs
+++ Assets/Scripts/GearScript.cs
@@ -8,37 +8,14 @@ public class GearScript : MonoBehaviour
     public LevelBuilderScript levelBuilderScript;
     public Transform mainHand;
     public Transform offHand;
-    public GameObject mainHandWeapon;
-    public GameObject offHandWeapon;
-    public WeaponScript mainHandWeaponScript;
-    public WeaponScript offHandWeaponScript;
-
-    IEnumerator WaitForWeapons()
-    {
-        while (!levelBuilderScript.finishedBuilding)
-        {
-            yield return null;
-        }
-
-        mainHandWeapon = mainHand.GetChild(0).gameObject;
-        offHandWeapon = offHand.GetChild(0).gameObject;
-        mainHandWeaponScript = mainHandWeapon.GetComponent<WeaponScript>();
-        offHandWeaponScript = offHandWeapon.GetComponent<WeaponScript>();
-
-        while (!(mainHandWeaponScript.IsFinishedBuilding() && offHandWeaponScript.IsFinishedBuilding()))
-        {
-            yield return null;
-        }
-
-        finishedBuilding = true;
-    }
-
+    
     void Start()
     {
+        // to-do: delete this script and clean up
         levelBuilder = GameObject.Find("Level Builder");
         levelBuilderScript = levelBuilder.GetComponent<LevelBuilderScript>();
         mainHand = this.transform.Find("Main Hand");
         offHand = this.transform.Find("Off Hand");
-        StartCoroutine(WaitForWeapons());
+        finishedBuilding = true;
     }
 }
